234b1dfa32c67300927b8387128dca5129c6750c
Jennifer Olsen <jenny@ninthdaydesign.com>
2013-12-15 20:03:30 -0800

    added canvas background

--- party/game.js
+++ party/game.js
@@ -8,8 +8,14 @@ game.update = function(dt) {
   }
 };
 game.draw = function() {
-  atom.context.fillStyle = 'black';
-  return atom.context.fillRect(0,0,atom.width,atom.height);
+  atom.context.beginPath();
+  atom.context.fillStyle = '#34e';
+  atom.context.fillRect(0,0, atom.width, atom.height/2);
+  atom.context.fillStyle = '#ee3';
+  atom.context.arc(140, atom.height/2 -30, 90, Math.PI*2, 0);
+  atom.context.fill();
+  atom.context.fillStyle = '#2e2';
+  atom.context.fillRect(0, atom.height/2, atom.width, atom.height/2);
 };
 window.onblur = function() {
   return game.stop();
